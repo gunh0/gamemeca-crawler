1bc6e34d1140ce44092ca3de54339c17f5d73174
Gunho Park <devgun@kakao.com>
2019-07-23 06:05:54 +0900

    commit

--- trees.gamemeca_paser.py
+++ trees.gamemeca_paser.py
@@ -5,18 +5,9 @@ from bs4 import BeautifulSoup
 req = requests.get('https://trees.gamemeca.com/gamerank/#1521881342690-f60aa3c1-8642')
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
-my_titles = soup.select(
-    'tr'
-    #'tr > td'
-    )
-# my_titles는 list 객체
-for title in my_titles:
-    # Tag안의 텍스트
-    print(title.text)
-    # Tag의 속성을 가져오기(ex: href속성)
-    print(title.get('href'))
 
+table_ranking = soup.findAll('td',{'class':'column-1'})
+table_titles = soup.findAll('td',{'class':'column-3'})  # table_titles list 객체
 
-#<tr class="row-2 even" role="row">
-#	<td class="column-1">1<p style="font-size:5px; color:#f84203;">(-)</p></td><td class="column-2"><a target="_blank" href="/leagueoflegends"><img src="/data/monthlyrank/pc/leagueoflegends.png" class="rankimg"></a></td><td class="column-3"><a target="_blank" href="/leagueoflegends">리그 오브 레전드</a></td><td class="column-4">라이엇 게임즈</td><td class="column-5">AOS</td>
-#</tr>
+for ranking, title in zip(table_ranking, table_titles):
+    print(ranking.get_text(), title.get_text())
